f615a8d8029b0b1037e015ce8df48df38dc1603a
ebin-mathews <ebin@jito.wtf>
2024-01-09 12:55:50 -0500

    test ci

--- src/jsonrpc_client/rpc_client.rs
+++ src/jsonrpc_client/rpc_client.rs
@@ -300,6 +300,7 @@ where
     Ok(encoded)
 }
 
+// Sample tests. Can use these as a reference point on how to use the api and what to expect
 #[cfg(test)]
 mod rpc_client_tests {
     use solana_program::hash::Hash;
